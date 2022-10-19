4d03c698b704203b1af9fa5cabc6dee678f6844a
Alex42bcn <111470634+Alex42bcn@users.noreply.github.com>
2022-10-19 12:22:59 +0000

    [FIX] aff.sh

---
+++ aff.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+echo 'Introduir el nom del fitxer amb la ruta a revisar'
+read nomfile
+if [ -e  $nomfile]
+then
+    echo "El fitxer $nomfile pertany al systema d'arxius"
+else
+    echo "El fitxer $nomfile NO es un fitxer del systema"
+fi
