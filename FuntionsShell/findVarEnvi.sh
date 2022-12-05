6e0c8c174189fdec1e4d1872e812c4fb98039ed0
Alex42bcn <alex.pic.42bcn@gmail.com>
2022-12-05 17:49:06 +0100

    [ADD] Funtion Folder

---
+++ FuntionsShell/findVarEnvi.sh
@@ -0,0 +1,25 @@
+# Crear Una funció que ens digui si una variable concreta ja es troba
+# definida com a variable d'entorn.
+# La funcio quedara definida deintre d'un script
+
+#! /bin/sh
+
+echo "What is the variable that find?"
+read var
+
+findenv()
+#This funtion needs an atribute wich is the one to be searched as env variable
+{
+env | grep -w "$1" > /dev/null 2> /dev/null
+result=$?
+ 
+if [ $result = "0" ];
+then
+echo "Variable encontrada"
+else
+echo "Variable no encontrada"
+fi
+}
+
+#. findenv
+findenv $var
