24ed48d970405210a62ff18d1f1760b6766dd7df
Alex42bcn <alex.pic.42bcn@gmail.com>
2022-12-07 00:37:42 +0100

    [ADD][FIX] Files shell

--- aff.sh
+++ aff.sh
@@ -1,4 +1,7 @@
+# Script per saber si un fitxer amb la seva ruta, introduït per teclat existeix o no.
+
 #! /bin/bash
+
 echo 'Introduir el nom del fitxer amb la ruta a revisar'
 read nomfile
 if [ -e  "$nomfile" ]
--- totalff.sh
+++ totalff.sh
@@ -1,4 +1,20 @@
 #! /bin/bash
-echo "Dona'm una ruta de dades"
-read ruta
 
+# Script per conèixer, d' una ruta facilitada per paràmetre, la xifra corresponent
+# a la suma de directoris i fitxers.
+
+echo "Dona'm una ruta de dades"
+read -r ruta
+
+num=$(ls -1 "$ruta" | wc -l | sed 's/ //g')
+total=$(find "$ruta" -type f -o -type d 2>/dev/null | wc -l | sed 's/ //g')
+#numfi=$(find "$ruta" -type f 2>/dev/null | wc -l | sed 's/ //g')
+#numfo=$(find "$ruta" -type d 2>/dev/null | wc -l | sed 's/ //g')
+
+echo "The number of files and folders in the rute $ruta is: $num"
+echo "Tankyou!"
+
+#sum=$(($numfi + $numfo))
+#echo "$sum"
+printf 'The number of files and folders in the rute (FIND) "%s" is: %d\n' "$ruta" "$total"
+ 
