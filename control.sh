006e23e554c17f6331ac66819a3da2f2e5b6e2eb
alex42bcn <alex.pic.42bcn@gmail.com>
2022-12-12 20:32:36 +0100

    [FIX] Refactor control.sh

--- control.sh
+++ control.sh
@@ -4,12 +4,23 @@
 # finalment, mostri per pantalla la informació relacionada amb el volum ja muntat. (Es a dir, que mostri l’hora de muntatge i
 # informació relacionada).
 
+# https://unix.stackexchange.com/questions/24182/how-to-get-the-complete-and-exact-list-of-mounted-filesystems-in-linux
+
+echo "The list of mount drivers is:\n"
+printf 'The name of mount-points in this syste is:\n%s\n' "$(findmnt -lo source,target,fstype,label,options,used -t ext4)"
+printf 'The name of mount-points in this syste is:\n%s\n' "$(findmnt -lo source -t ext4)"
+
 findmnt -lo source,target,fstype,label,options,used -t ext4 > /Users/alpicany/logs/mount.log
+findmnt -lo source,target,fstype,label,options,used -t ext4
+findmnt -Jo source,target,label,uuid -t ext4
 
 df | awk '{print $9}' | grep -Ex "/Volumes/myvolume"
 df | awk '{print $9}' | grep -Ex "/Users/alpicany"
 
 lsblk -f  #In linux llista el UUID (Nom del volum) del volums montats
+
+#to find out the time
+sudo tune2fs -l /dev/sda2 | grep "Last mount time"
 
 LOCALMOUNTPOINT="/folder/share"
 
